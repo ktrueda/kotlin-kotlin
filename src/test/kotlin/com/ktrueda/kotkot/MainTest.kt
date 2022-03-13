f8cd3060c96a3084d5960bb8d3878982ff449d6c
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-03-13 17:35:19 +0900

    can run test

---
+++ src/test/kotlin/com/ktrueda/kotkot/MainTest.kt
@@ -0,0 +1,25 @@
+package com.ktrueda.kotkot
+
+import org.junit.jupiter.api.Test
+import java.io.File
+
+val skipCompile = true
+
+class MainTest {
+    @Test
+    fun can_run() {
+        // run kotlinc
+        if (!skipCompile) {
+            ProcessBuilder().command(listOf("sh", "build.sh"))
+                .directory(File("./src/test/resources"))
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start()
+                .waitFor();
+        }
+
+        val cf: ClassFile = ClassFile.load(File("./target/src/HelloWorldKt.class"))
+        val executor = Executor(cf)
+        executor.runMain()
+    }
+}
