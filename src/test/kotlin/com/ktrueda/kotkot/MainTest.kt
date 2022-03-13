39d341b8394c89dd7259b9bce92e7df894ee92ce
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-03-13 18:21:58 +0900

    Test factory

--- src/test/kotlin/com/ktrueda/kotkot/MainTest.kt
+++ src/test/kotlin/com/ktrueda/kotkot/MainTest.kt
@@ -1,31 +1,40 @@
 package com.ktrueda.kotkot
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import java.io.File
+import kotlin.streams.asStream
+
 
 val skipCompile = true
 
 class MainTest {
-    @Test
-    fun can_run() {
-        // run kotlinc
-        if (!skipCompile) {
-            ProcessBuilder().command(listOf("sh", "build.sh"))
-                .directory(File("./src/test/resources"))
-                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-                .redirectError(ProcessBuilder.Redirect.INHERIT)
-                .start()
-                .waitFor();
-        }
 
-        File("./target/src/").walk()
-            .map { it.toPath() }
-            .filter { it.toAbsolutePath().toString().endsWith(".kt") }
-            .map { it.fileName.toString().split('.')[0] }
-            .forEach {
+    companion object {
+        @BeforeAll
+        fun compile() {
+            if (!skipCompile) {
+                ProcessBuilder().command(listOf("sh", "build.sh"))
+                    .directory(File("./src/test/resources"))
+                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                    .redirectError(ProcessBuilder.Redirect.INHERIT)
+                    .start()
+                    .waitFor();
+            }
+        }
+    }
+
+    @TestFactory
+    fun runAllTestFiles() = File("./target/src/").walk()
+        .map { it.toPath() }
+        .filter { it.toAbsolutePath().toString().endsWith(".kt") }
+        .map { it.fileName.toString().split('.')[0] }
+        .map {
+            DynamicTest.dynamicTest(it) {
                 val cf: ClassFile = ClassFile.load(File("./target/src/${it}Kt.class"))
                 val executor = Executor(cf)
                 executor.runMain()
             }
-    }
+        }.asStream()
 }
